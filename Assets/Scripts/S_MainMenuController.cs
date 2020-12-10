cc34fa0c799b5e7f5e030e140e1ebe65f92239f5
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-10 18:24:26 +0530

    Functional UI System
    Updates to the UI in all scenes in the project to make it functional.

--- Assets/Scripts/S_MainMenuController.cs
+++ Assets/Scripts/S_MainMenuController.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class S_MenuController : MonoBehaviour
+public class S_MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Buttons in the Main Menu
+    /*public Button playButton;
+    public Button helpButton;
+    public Button exitButton;*/
+    public GameObject rules;
+
+    private void Start()
     {
-        
+        // Hide rules by default on start
+        if (rules != null)
+        {
+            rules.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Open the game when the player clicks on Play Button
+    public void ClickPlay()
     {
-        
+        SceneManager.LoadScene("Sc_Game"); // Load Game Scene
+    }
+
+    // Open the help scene when player clicks the Help Button
+    public void ClickHelp()
+    {
+        SceneManager.LoadScene("Sc_Help"); // Load Help Scene
+    }
+
+    // Go back to Main Menu when player clicks of Back Button
+    public void ClickBack()
+    {
+        SceneManager.LoadScene("Sc_MainMenu"); // Load Main Menu Scene
+    }
+
+    // Display rules when the player clicks on Rule Button
+    public void ClickRules()
+    {
+        rules.SetActive(!rules.activeSelf);
+    }
+
+    // Quit game when user clicks the Exit Button
+    public void ClickExit()
+    {
+        Application.Quit(); // Quit application (will not work in editor)
     }
 }
