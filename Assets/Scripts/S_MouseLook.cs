236da7c88d0062f2e1d67b48a932a24c16143d38
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-11 16:20:33 +0530

    FPS Camera Look-At
    Added basic FPS camera setup

---
+++ Assets/Scripts/S_MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_MouseLook : MonoBehaviour
+{
+    public float mouseSnsty = 100.0f;
+    public Transform playerBody;
+
+    private float xRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSnsty * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSnsty * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
