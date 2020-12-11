1fbdb2a360f5353f6c784163154f8619b051bc31
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-11 15:45:14 +0530

    Basic timer added
    Count-up timer

--- Assets/Scripts/S_HighlightController.cs
+++ Assets/Scripts/S_HighlightController.cs
@@ -5,6 +5,8 @@ public class S_HighlightController : MonoBehaviour
 {
     public Image[] discIndicators = new Image[5]; // All 5 disc's indicators
     public Image[] towerIndicators = new Image[3]; // All 3 tower's indicators
+    public int selectedDisc = -1; // Number of Disc that is currently selected, -1 if none
+    public int selectedTower = -1; // Number of Tower that is currently selected, -1 if none
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,7 @@ public class S_HighlightController : MonoBehaviour
         }
     }
 
-    public void HighlightObject(bool isDisc, int indicatorNum)
+    /*public void HighlightObject(bool isDisc, int indicatorNum)
     {
         // Check if the gameobject is a disc
         if (isDisc)
@@ -33,5 +35,5 @@ public class S_HighlightController : MonoBehaviour
         {
             towerIndicators[indicatorNum].gameObject.SetActive(true); // Highlight the tower
         }
-    }
+    }*/
 }
---
+++ Assets/Scripts/S_TimeController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class S_TimeController : MonoBehaviour
+{
+    public Text time; // Timer that counts up
+    public Text movesMade; // Shows the number of moves made by user
+    public int moves; // Moves made counter
+
+    private float seconds = 0.0f;
+    private float minutes = 0.0f;
+
+    void Update()
+    {
+        // Use Time.deltaTime to have a clock
+        seconds += Time.deltaTime;
+        if (seconds > 60)
+        {
+            // Increment minutes every 60 seconds
+            minutes += 1;
+            seconds = 0;
+        }
+
+        time.text = minutes + ":" + seconds;
+
+    }
+}
