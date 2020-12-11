f85b150bab58dbe6f17f9b27bbc1606c4600f097
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-11 16:26:11 +0530

    FPS Character Movement implemented
    Basic FPS Character movement using WASD keys

--- Assets/Scripts/S_PlayerMovement.cs
+++ Assets/Scripts/S_PlayerMovement.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class S_PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    public float speed = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
