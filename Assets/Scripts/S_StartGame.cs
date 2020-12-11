30742e8cb3b636500f13a2da5cfd08938afcc75b
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-11 17:26:39 +0530

    FPS updates and Game Start
    Implemented ability to change from FPS to point and click

---
+++ Assets/Scripts/S_StartGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_StartGame : MonoBehaviour
+{
+    public Transform position; // Position that camera needs to go to
+    public Camera mainCamera; // Main camera in scene
+    public GameObject player; // Player character in scene
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered...");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Starting Game...");
+
+            mainCamera.gameObject.GetComponent<S_MouseLook>().enabled = false; // Deactivate Script
+            Cursor.lockState = CursorLockMode.None; // Unlock cursor
+            mainCamera.transform.SetParent(null); // Make camera independant
+
+            // Change camera's transform
+            mainCamera.transform.position = position.position;
+            mainCamera.transform.rotation = position.rotation;
+
+            // Deactivate player
+            player.SetActive(false);
+            mainCamera.gameObject.SetActive(true);
+        }
+    }
+}
