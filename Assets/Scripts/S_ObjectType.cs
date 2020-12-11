a8dc8f372e5603fff68fdcf90f153265519e76dc
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-11 12:49:04 +0530

    Minor changes to scripts

--- Assets/Scripts/S_ObjectType.cs
+++ Assets/Scripts/S_ObjectType.cs
@@ -7,4 +7,6 @@ public class S_ObjectType : MonoBehaviour
     // Tells you whether this gameObject is a disc or tower and it's color
     public bool isDisc; // True is this gameObject is a Disc and false if it is a Tower
     public int objectNum; // Number of Disc/Tower
+    public bool isSelected; // True if user has selected this GameObject
+    public Stack<int> discStack; // Only for Towers. Contains the disc that are currently in this tower
 }
--- Assets/Scripts/S_SelectObject.cs
+++ Assets/Scripts/S_SelectObject.cs
@@ -3,26 +3,33 @@ using UnityEngine.UI;
 
 public class S_SelectObject : MonoBehaviour
 {
-    public GameObject highlight; // Image component attached to this object
+    public GameObject highlight; // Image attached to this object
 
     // Get the Image component on Start
     /*private void Start()
     {
-        highlight = transform.GetChild(3).GetChild(0).gameObject; // Get Image attached to object
+        highlight = transform.GetChild(3).gameObject; // Get Image attached to object
         Debug.Log("Got Image...");
     }*/
 
     // Check if user interacts with this object
     private void OnMouseOver()
     {
-        Debug.Log("Mouse Over...");
+        //Debug.Log("Mouse Over...");
 
         // Check if user clicks on object
         if (Input.GetMouseButtonDown(0))
         {
             // Highlight object if user clicks on it
             highlight.SetActive(true);
+            this.GetComponent<S_ObjectType>().isSelected = true; // Update object selection
             Debug.Log("Highlighted...");
         }
+    }
+
+    // Move Disc to the another tower
+    private void MoveDisc()
+    {
+
     }
 }
