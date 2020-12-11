b33efc01cbc98a9792717a96d1cd4cbf08e42885
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-11 18:31:51 +0530

    Minor updates to MoveController
    - Renamed S_MoveDisc to S_MoveController

---
+++ Assets/Scripts/S_MoveController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class S_MoveController : MonoBehaviour
+{
+    const int NUM_DISCS = 5; // Total number of Discs
+
+    public Transform[] spawnPointsOne = new Transform[NUM_DISCS]; // Spawn points on Tower One
+    public Transform[] spawnPointsTwo = new Transform[NUM_DISCS]; // Spawn points on Tower Two
+    public Transform[] spawnPointsThree = new Transform[NUM_DISCS]; // Spawn points on Tower Three
+    public GameObject[] discs = new GameObject[NUM_DISCS]; // All discs in game
+    public Transform newDiscPosition; // New position of Disc
+
+
+}
--- Assets/Scripts/S_ObjectType.cs
+++ Assets/Scripts/S_ObjectType.cs
@@ -8,5 +8,6 @@ public class S_ObjectType : MonoBehaviour
     public int objectNum; // Number of Disc/Tower
     public bool isSelected; // True if user has selected this GameObject
     public bool isTop; // Only for disc. This disc is the topmost disc in a tower
+    public int currentTower; // Only for Disc. This is the current tower the disc is on
     public Stack<int> discStack = new Stack<int>(5); // Only for Towers. Contains the disc that are currently in this tower
 }
