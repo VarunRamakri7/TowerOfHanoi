86a222a6e883fc6a7395bfff5a5b0a41a11d379e
Varun Ramakrishnan <oceans.varun@gmail.com>
2020-12-11 12:09:58 +0530

    Highlight GameObject
    - Minor material changes
    - Basic script to highlight selected disc or tower

---
+++ Assets/Scripts/S_MoveDiscController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class S_MoveDiscController : MonoBehaviour
+{
+    public static Image[] discIndicators = new Image[5]; // All 5 disc's indicators
+    public static Image[] towerIndicators = new Image[3]; // All 3 tower's indicators
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide all disc indicators
+        foreach(Image image in discIndicators)
+        {
+            image.gameObject.SetActive(false);
+        }
+        
+        // Hide all tower indicators
+        foreach (Image image in towerIndicators)
+        {
+            image.gameObject.SetActive(false);
+        }
+    }
+
+    public static void HighlightObject(bool isDisc, int indicatorNum)
+    {
+        // Check if the gameobject is a disc
+        if (isDisc)
+        {
+            discIndicators[indicatorNum].gameObject.SetActive(true); // Highlight the disc
+        }
+        else
+        {
+            towerIndicators[indicatorNum].gameObject.SetActive(true); // Highlight the tower
+        }
+    }
+}
